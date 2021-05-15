2f29fe63bbead121675a3c0ecd364a329f69e160
ViniSilva32 <78766084+ViniSilva32@users.noreply.github.com>
2021-05-14 22:01:23 -0300

    Robot
    aula Robot 14/05/2021

---
+++ RobotGuardBTStarter/Assets/AI.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using Panda;
+
+public class AI : MonoBehaviour
+{
+    public Transform player;        // movimentação do player
+    public Transform bulletSpawn;   // local de onde a bala sai
+    public Slider healthBar;        // seta a barra de vida do bot
+    public GameObject bulletPrefab; // seta o prefab dos disparos do player
+
+    NavMeshAgent agent;
+    public Vector3 destination; // aponta a direção ao qual o bot deve ir
+    public Vector3 target;      // mira 
+    float health = 100.0f;      // vida maxima do bot
+    float rotSpeed = 5.0f;      //velocidade de rotação do bot
+
+    float visibleRange = 80.0f; //  distancia máxima ao qual é visivel
+    float shotRange = 40.0f;    // distancia maxima do disparo    
+
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = shotRange - 5;         //velocidade dos disparos
+        InvokeRepeating("UpdateHealth",5,0.5f); 
+    }
+
+    void Update()
+    {
+        Vector3 healthBarPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        healthBar.value = (int)health;                                                      // faz com que a barra de vida esteja sempre seguindo o BOT
+        healthBar.transform.position = healthBarPos + new Vector3(0,60,0);
+    }
+
+    void UpdateHealth()
+    {
+       if(health < 100)             // O HP volta depois de um tempo fora de combate
+            health ++;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "bullet")
+        {
+            health -= 10;           //Quantidade de HP removida pelos tiros do player
+        }
+    }
+    [Task]
+    public void PickRandomDestination()     //Escolhe uma direção aleatória para ir
+    {
+        Vector3 dest = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100));
+        agent.SetDestination(dest);
+        Task.current.Succeed();
+    }
+    [Task]
+    public void MoveToDestination() // move o bot para a direção escolhida
+    {
+        if (Task.isInspected)
+            Task.current.debugInfo = string.Format("t={0:0.00}", Time.time);
+        if(agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+        {
+            Task.current.Succeed();
+        }
+    }
+}
+
---
+++ RobotGuardBTStarter/Assets/Drive.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour {
+
+	float speed = 20.0F;        //velocidade de movimento
+    float rotationSpeed = 120.0F;  //velocidadede rotação
+    public GameObject bulletPrefab;   //seta o prefab dos tiros
+    public Transform bulletSpawn;       // local de onde o tiro sai
+
+    void Update() {
+        float translation = Input.GetAxis("Vertical") * speed;  //botões que movem o tanque para frente e para trás
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;   //botões que fazem a rotação do tanque
+        translation *= Time.deltaTime;  
+        rotation *= Time.deltaTime;     
+        transform.Translate(0, 0, translation);     //movimentação do tanque
+        transform.Rotate(0, rotation, 0);       // rotação do tanque
+
+        if(Input.GetKeyDown("space")) // aperte espaço para disparar os projeteis
+        {
+            GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
+            bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward*2000);
+        }
+    }
+}
